10716916ed52052ff9367db03420ccb788928d92
Yousef Khalid <vixer450@gmail.com>
2019-07-30 17:23:02 +0300

    adding urls login and logout

--- haraj/settings.py
+++ haraj/settings.py
@@ -25,7 +25,7 @@ STATICFILES_DIRS=[
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/user_login'
+LOGIN_URL = '/login'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
